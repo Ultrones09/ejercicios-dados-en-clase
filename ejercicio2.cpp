8b25427690b30bab23110e0b395dac75f49cc22c
Ultrones09 <gadiel.gonzalezp@gmail.com>
2024-09-25 07:10:23 -0500

    Add files via upload

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -17,3 +17,24 @@ int main(){
     cout<<"El factor primo grande es: "<<numero<<endl;
     return 0;
 }
+
+/*long long mayorFactorPrimo(long long numero) {
+    int factor {2};
+
+    while (factor*factor<=numero) {
+        if (numero%factor==0) {
+            numero/=factor; 
+        } else {
+            factor++;
+        }
+    }
+    return numero;
+}
+
+int main() {
+    long long numero = 600851475143;
+
+    cout << "El mayor factor primo es: " << mayorFactorPrimo(numero) << endl;
+
+    return 0;
+}*/
--- ejrcicio1.cpp
+++ ejrcicio1.cpp
@@ -24,3 +24,31 @@ int main(){
     cout<<endl<<"resultado:"<<suma;
     return 0;
 }
+
+/*void SumaFibo(int n) {
+    int a{0};
+    int b{1};
+    int c{0};
+    int suma{0};
+
+    for (int i=0; i<n; i++){
+        c=a+b;
+        a=b;
+        b=c;
+        cout<<c<<" ";
+        
+        if(c%2==0){
+            suma+=c;
+        }
+    }
+    cout<<endl<<"Resultado: "<<suma<<endl;
+}
+
+int main() {
+    int n;
+    cout<<"Ingresar el numero: ";
+    cin>>n;
+
+    SumaFibo(n);
+    return 0;
+}*/
